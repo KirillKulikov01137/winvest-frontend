c6b1119be237488ba84030dae1512f611a747a64
kulisk <kirillka.kulikov@mail.ru>
2021-11-09 15:28:32 +0400

    Code cleanup

--- src/pages/stock-page/StockPage.tsx
+++ src/pages/stock-page/StockPage.tsx
@@ -1,11 +1,6 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC} from 'react';
 import Container from "../../components/container/Container";
-import PageHeader from "../../components/page-header/PageHeader";
-import {ParamsI} from "./ParamsInterface";
-import {useParams} from 'react-router-dom';
 import {StyledStockPage} from "./StyledStockPage";
-import Graphic from '../../Graphics';
-import {useTypedSelector} from '../../hooks/useTypedSelector';
 import StockScreen from '../../components/stock-screen/StockScreen';
 
 const StockPage: FC = () => {
